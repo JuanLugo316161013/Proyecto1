2f49ca0bf68be37563bb5c1a5c3048eaf742d488
JuanLugo316161013 <juanlugo@ciencias.unam.mx>
2019-03-14 10:47:06 -0600

    Proyecto1 maven corregido

--- proyecto1/src/main/java/mx/unam/ciencias/edd/proyecto1/Proyecto1.java
+++ proyecto1/src/main/java/mx/unam/ciencias/edd/proyecto1/Proyecto1.java
@@ -8,6 +8,6 @@ public class Proyecto1
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Madafaka!" );
+        System.out.println( "Hello Madafaka" );
     }
 }
