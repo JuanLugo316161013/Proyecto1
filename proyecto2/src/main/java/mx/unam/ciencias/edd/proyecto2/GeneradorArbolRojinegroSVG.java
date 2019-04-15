f49de324817136abc2766d96d3dbd6546afcba9d
JuanLugo316161013 <juanlugo@ciencias.unam.mx>
2019-04-15 12:28:40 -0500

    Eliminando elementos inecesarios

--- proyecto2/src/main/java/mx/unam/ciencias/edd/proyecto2/GeneradorArbolRojinegroSVG.java
+++ proyecto2/src/main/java/mx/unam/ciencias/edd/proyecto2/GeneradorArbolRojinegroSVG.java
@@ -2,6 +2,7 @@ package mx.unam.ciencias.edd.proyecto2;
 
 import mx.unam.ciencias.edd.ArbolBinario;
 import mx.unam.ciencias.edd.ArbolRojinegro;
+import mx.unam.ciencias.edd.Color;
 import mx.unam.ciencias.edd.Lista;
 /**
  * Genera codigo SVG que representa a un ArbolRojinegro.
@@ -22,5 +23,30 @@ public class GeneradorArbolRojinegroSVG extends GeneradorArbolBinarioSVG {
 	 */
 	@Override protected ArbolBinario<Integer> nuevoArbolBinario() {
 		return new ArbolRojinegro<Integer>();
+	}
+
+	@Override protected String vertice(int x, int y, int radio, int distancia, VerticeArbolBinario<Integer> vertice) {
+		double trazo = 3;
+		double texto = 20;
+
+		if (radio < 20) {
+			trazo = 1.5;
+			texto = 17.5;
+		}
+
+		String verticeSVG = "";
+		if (vertice.hayIzquierdo())
+			verticeSVG += String.format("<line x1='%d' y1='%d' x2='%d' y2='%d' stroke='black' stroke-width='3' />\n",
+				x, y, x - distancia, y + 120);
+
+		if (vertice.hayDerecho())
+			verticeSVG += String.format("<line x1='%d' y1='%d' x2='%d' y2='%d' stroke='black' stroke-width='3' />\n",
+				x, y, x + distancia, y + 120);
+
+		verticeSVG += String.format("<circle cx='%d' cy='%d' r='%d' stroke='black' stroke-width='%.1f' fill='white' />\n",
+			x, y, radio, trazo);
+
+		return verticeSVG += String.format("<text fill='black' font-family='sans-serif' font-size='%.1f' x='%d' y='%d' text-anchor='middle'>%d</text>\n",
+			texto, x, y + 5, vertice.get());
 	}
 }
